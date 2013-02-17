5d1a368e27cedf6666b3cb85d0746c6fb3d82d7b
Paul Lamere <Paul.Lamere@gmail.com>
2013-02-16 19:09:12 -0800

    Checkpoint

---
+++ js/favsongs.js
@@ -0,0 +1,38 @@
+var favSongs = [
+    {
+        "artist": "Adele", 
+        "id": "TRSBVUT12F87DF0212", 
+        "title": "Rolling in the Deep", 
+        "visits": 62
+    }, 
+    {
+        "artist": "Muse", 
+        "id": "TRHIUST13B5695A3E4", 
+        "title": "Madness", 
+        "visits": 62
+    }, 
+    {
+        "artist": "The White Stripes", 
+        "id": "TRMQYSY13B4C10127E", 
+        "title": "Seven Nation Army", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Daft Punk", 
+        "id": "TRSRSLI13451D2B044", 
+        "title": "Around The World", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Skrillex", 
+        "id": "TRECVNM13B542388C0", 
+        "title": "Scary Monsters and Nice Sprites", 
+        "visits": 25
+    }, 
+    {
+        "artist": "The Postal Service", 
+        "id": "TRBXFDO13B52FCCD0B", 
+        "title": "Such Great Heights", 
+        "visits": 23
+    }, 
+]
